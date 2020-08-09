f4b01f3c149749a5da9f62133e23fc6f5db6c8de
Daniel Sullivan <dsullivan@charm.co.jp>
2020-08-10 08:59:13 +0900

    Export error details

--- error.go
+++ error.go
@@ -9,9 +9,9 @@ import (
 
 // HTTPError is used for errors
 type HTTPError struct {
-	err    error
-	msg    string
-	status int
+	Err    error
+	Msg    string
+	Status int
 	Errors []Error `json:"errors,omitempty"`
 }
 
@@ -22,7 +22,7 @@ type HTTPError struct {
 type Error struct {
 	ID     string       `json:"id,omitempty"`
 	Links  *ErrorLinks  `json:"links,omitempty"`
-	Status string       `json:"status,omitempty"`
+	Status string       `json:"Status,omitempty"`
 	Code   string       `json:"code,omitempty"`
 	Title  string       `json:"title,omitempty"`
 	Detail string       `json:"detail,omitempty"`
@@ -53,7 +53,7 @@ type ErrorSource struct {
 // marshalHTTPError marshals an internal httpError
 func marshalHTTPError(input HTTPError) string {
 	if len(input.Errors) == 0 {
-		input.Errors = []Error{{Title: input.msg, Status: strconv.Itoa(input.status)}}
+		input.Errors = []Error{{Title: input.Msg, Status: strconv.Itoa(input.Status)}}
 	}
 
 	data, err := json.Marshal(input)
@@ -66,18 +66,18 @@ func marshalHTTPError(input HTTPError) string {
 	return string(data)
 }
 
-// NewHTTPError creates a new error with message and status code.
-// `err` will be logged (but never sent to a client), `msg` will be sent and `status` is the http status code.
-// `err` can be nil.
+// NewHTTPError creates a new error with message and Status code.
+// `Err` will be logged (but never sent to a client), `Msg` will be sent and `Status` is the http Status code.
+// `Err` can be nil.
 func NewHTTPError(err error, msg string, status int) HTTPError {
-	return HTTPError{err: err, msg: msg, status: status}
+	return HTTPError{Err: err, Msg: msg, Status: status}
 }
 
-// Error returns a nice string represenation including the status
+// Error returns a nice string represenation including the Status
 func (e HTTPError) Error() string {
-	msg := fmt.Sprintf("http error (%d) %s and %d more errors", e.status, e.msg, len(e.Errors))
-	if e.err != nil {
-		msg += ", " + e.err.Error()
+	msg := fmt.Sprintf("http error (%d) %s and %d more errors", e.Status, e.Msg, len(e.Errors))
+	if e.Err != nil {
+		msg += ", " + e.Err.Error()
 	}
 
 	return msg
